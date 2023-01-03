289ef2f5bddce0033e782378f56ee772cd1cc642
absolutholz <absolutholz@gmail.com>
2023-01-03 18:28:33 +0100

    fix: update usermgmt typescript issue

--- src/components/UserMgmt/_UserMgmt.tsx
+++ src/components/UserMgmt/_UserMgmt.tsx
@@ -3,34 +3,34 @@ import type { IUserMgmt } from './_UserMgmt.annotations';
 import { useUserMgmt } from './_useUserMgmt';
 
 export function UserMgmt({ onLogIn, onLogOut }: IUserMgmt): JSX.Element {
-  const { player } = usePlayerContext();
-  const { promptLogIn, logOut } = useUserMgmt();
+	const { player } = usePlayerContext();
+	const { promptLogIn, logOut } = useUserMgmt();
 
-  const handleLogIn = () => {
-    const player = promptLogIn();
-    if (player !== undefined && onLogIn) {
-      onLogIn(player);
-    }
-  };
+	const handleLogIn = () => {
+		const player = promptLogIn();
+		if (player !== undefined && onLogIn) {
+			onLogIn(player);
+		}
+	};
 
-  const handleLogOut = () => {
-    if (player !== undefined) {
-      logOut();
-      onLogOut && onLogOut(player);
-    }
-  };
+	const handleLogOut = () => {
+		if (player !== null) {
+			logOut();
+			onLogOut && onLogOut(player);
+		}
+	};
 
-  return (
-    <>
-      {player?.name ? (
-        <button onClick={handleLogOut} type="button">
-          Log out
-        </button>
-      ) : (
-        <button onClick={handleLogIn} type="button">
-          Log in
-        </button>
-      )}
-    </>
-  );
+	return (
+		<>
+			{player?.name ? (
+				<button onClick={handleLogOut} type="button">
+					Log out
+				</button>
+			) : (
+				<button onClick={handleLogIn} type="button">
+					Log in
+				</button>
+			)}
+		</>
+	);
 }
