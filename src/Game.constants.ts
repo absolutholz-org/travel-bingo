f5b0eab3728674ffbcbd2ec94aaeb7aa3dcab327
absolutholz <absolutholz@gmail.com>
2023-01-27 20:18:35 +0100

    chore: update baseurl and remove symbol src slash

--- src/Game.constants.ts
+++ src/Game.constants.ts
@@ -1,7 +1,7 @@
 export const STORAGE_PREFIX = 'absolutholzArcade_travelBingo';
 export const MINIMUM_PLAYERS_REQUIRED = 2;
 export const MAXIMUM_PLAYERS_ALLOWED = 4;
-export const SIGN_DIRECTORY = '/signs/optimized/';
+export const SIGN_DIRECTORY = 'signs/optimized/';
 
 export const WINNING_COMBINATIONS = ['rows', 'columns', 'diagonals', 'corners'];
 // Object.freeze(WINNING_COMBINATIONS);
