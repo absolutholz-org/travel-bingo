d5118ebec1c9b308ee880b791fb3d386af23514d
absolutholz <absolutholz@gmail.com>
2023-02-03 20:02:34 +0100

    feat: allow single player game

--- src/Game.constants.ts
+++ src/Game.constants.ts
@@ -1,5 +1,5 @@
 export const STORAGE_PREFIX = 'absolutholzArcade_travelBingo';
-export const MINIMUM_PLAYERS_REQUIRED = 2;
+export const MINIMUM_PLAYERS_REQUIRED = 1;
 export const MAXIMUM_PLAYERS_ALLOWED = 4;
 export const SIGN_DIRECTORY = './signs/optimized/';
 
